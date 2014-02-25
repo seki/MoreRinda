bd18c1507134cf0e3a99aa3ccf741288bac8c8f1
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2014-02-25 11:38:23 +0900

    add lib/rinda

--- install.rb
+++ install.rb
@@ -2,7 +2,10 @@ require 'rbconfig'
 require 'fileutils'
 
 dest = RbConfig::CONFIG['sitelibdir'] + '/rinda'
-src = ['lib/rinda/eval.rb']
+src = %w(lib/rinda/eval.rb		lib/rinda/tokyotuplestore.rb
+lib/rinda/inspect.rb		lib/rinda/tuplestore.rb
+lib/rinda/njet.rb		lib/rinda/version.rb
+lib/rinda/ptuplespace.rb)
 
 FileUtils.mkdir_p(dest, {:verbose => true})
 src.each do |s|
